bccc838a1d52483ca7049cb2db1528695dfaeaac
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-12-05 22:21:37 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Product Landing Page
+
+**Note**: currently in development (work in progress).
+
+## About
+
+This is a sample product landing page conforming to the HTML/CSS requirements
+of [FCC]()'s [*Build a Product Landing
+Page*](https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/build-a-product-landing-page)
+project, which is part of the [*Responsive Web Design
+Certification*](https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects)
+program.
+
+The HTML/CSS requirements are currently being tested using Pytest and Beautiful
+Soup 4, to make sure they are not broken, while designing the web page.
+
+## Running the tests
+
+### Requirements for the tests
+
+You'd need the libraries specified in the `requirements.txt` file. You
+can obtain them by running the following command in your terminal `pip install
+-r requirements.txt`; however, please note this will install the libraries in
+reference globally.
+
+### Execute the tests
+
+From your terminal, run the following command: `pytest --verbose`.
+
+Unless more tests are scheduled to be written, all tests should pass.
---
+++ test_html_requirements.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup as bs
+import pytest
+
+
+@pytest.fixture(scope='module')
+def html_soup(html_file="landing_page.html"):
+    """Pass the bs4 html parsed file object to all tests."""
+    with open(html_file) as html:
+        soup = bs(html, 'html.parser')
+        yield soup
+        print("Closing file!")
+    print("File closed!")
+
+
+def test_title(html_soup):
+    """Test title string."""
+    assert "Landing Page" == html_soup.title.string
+
+
+def test_header_requirements(html_soup):
+    """Test header requirements from User Stories 1 to 4."""
+    assert html_soup.header
+    assert html_soup.header['id'] == "header"                           # US1
+    assert html_soup.header.img                                         # US2A
+    assert html_soup.header.img['id'] == 'header-img'                   # US2B
+    assert html_soup.header.nav                                         # US3A
+    assert html_soup.header.nav['id'] == 'nav-bar'                      # US3B
+    nav_links = html_soup.find_all(attrs={"class": "nav-link"})
+    assert nav_links                                                    # US4A
+    assert 'class="nav-link"' in str(html_soup.find_all('nav')[0])      # US4B
+    assert len(nav_links) >= 3
+
+
+def test_form_requirements(html_soup):
+    """Test form requirements from User Stories 7 to 12."""
+    assert html_soup.form                                               # US7A
+    assert html_soup.form['id'] == 'form'                               # US7B
+    email_input = html_soup.form.find(attrs={'type': 'email'})
+    assert email_input                                                  # US8
+    assert "email" in email_input['placeholder']                        # US9
+    assert html_soup.form.find(attrs={'id': 'email'})                   # US10
+    submit_input = html_soup.form.find(attrs={'type': 'submit'})
+    assert submit_input['id'] == 'submit'                               # US11
+    assert "https://" in html_soup.form['action']                       # US12
+    
+
+def test_voluntary_failure():
+    """Raise Error if tests are still pending.
+    TODO: US5 - nav-link redirects to corresponding section
+    US6 - embedded video with id='video'
+    US13 - navbar should always be on top
+    US14 - at least one media query
+    US15 - should utilize CSS Flexbox at least once"""
+    raise AssertionError("Finish your tests!")
